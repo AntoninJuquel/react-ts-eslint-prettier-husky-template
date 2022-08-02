d31f25208e22d1fe9b392783a7c925e8152117a9
Antonin <antonin.juquel@outlook.fr>
2022-08-02 20:28:30 +0200

    End of init

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { App } from '../views'
 
-test('renders learn react link', () => {
+test('renders Hello world !', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const hello = screen.getByText(/Hello world !/i)
+  expect(hello).toBeInTheDocument()
 })
