e746f7785a2a47282b7bb9f621a329f9f36225be
Antonin <antonin.juquel@outlook.fr>
2022-09-09 18:28:14 +0200

    fix: language dir

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -16,6 +16,9 @@ i18n
     },
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath: '/react-ts-eslint-prettier-husky-template/locales/{{lng}}/{{ns}}.json'
     }
   });
 
