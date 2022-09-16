73f47a420dad54961fdef18fa43f9e32deec1849
Antonin <antonin.juquel@outlook.fr>
2022-09-16 20:13:23 +0200

    test: added dropdown test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -6,3 +6,11 @@ test('Hello world on screen', () => {
 
   expect(screen.getByText('Hello world !')).toBeDefined();
 });
+
+test('Two dropdowns on screen', () => {
+  render(<App />);
+
+  const lists = screen.getAllByRole('list');
+
+  expect(lists.length).toBe(2);
+});
